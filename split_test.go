e7cc1ceac53c03fc595d8cf67e5363f0dca5780f
kubasiemion <kuba.siemion@gmail.com>
2023-05-26 12:17:45 +0200

    Proper decrypt

--- split_test.go
+++ split_test.go
@@ -18,26 +18,53 @@ func TestSplit(t *testing.T) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println("D:", privkey.D)
-	s, err := gf256.SplitBytes(privkey.D.Bytes(), shares, threshold)
+	s, err := gf256.SplitBytes(privkey.D.Bytes(), 5, 3)
+	if err != nil {
+		t.Error(err)
+	}
 
+	b, err := gf256.RecoverBytes(s[2:])
+	if err != nil {
+		t.Error(err)
+	}
+	D1 := new(big.Int).SetBytes(b)
+	if privkey.D.Cmp(D1) != 0 {
+		t.Errorf("Wrong D recovered: /n%v/n%v", privkey.D, D1)
+	}
+
+}
+
+func TestDecrypt(t *testing.T) {
+	privkey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		fmt.Println(err)
+	}
+	pass := []byte("aaaaaa")
 	kdf := "scrypt"
 	encalg := "aes-128-ctr"
 	keyfile := common.Keyfile{}
 	crypto := &keyfile.Crypto
 	crypto.Kdf = kdf
 	crypto.Cipher = encalg
-	err = common.EncryptAES(&keyfile, s[0].Value, pass)
+	err = common.EncryptAES(&keyfile, privkey.D.Bytes(), pass)
 	if err != nil {
-		fmt.Println(err)
+		t.Error(err)
 	}
-	shareRaw, err := common.DecryptAES(&keyfile, pass)
-
-	privDBytes, err := gf256.RecoverBytes(s[2:4])
+	key, err := keyfile.KeyFromPass(pass)
 	if err != nil {
-		fmt.Println(err)
+		t.Errorf("KDF fail: %s", err)
 	}
-	privKD := new(big.Int).SetBytes(privDBytes)
-	fmt.Println("D:", privKD)
+	err = keyfile.VerifyMAC(key)
+	if err != nil {
+		t.Error("Wrong MAC")
+	}
+	b, err := common.Decrypt(&keyfile, key)
+	if err != nil {
+		t.Error(err)
+	}
+	D1 := new(big.Int).SetBytes(b)
+	if privkey.D.Cmp(D1) != 0 {
+		t.Errorf("Wrong D recovered: /n%v/n%v", privkey.D, D1)
+	}
 
 }
